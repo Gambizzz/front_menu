308b452d845515a07dd15a069b913e9bf5d6d670
Gambizzz <anelisegambini@yahoo.fr>
2024-06-19 20:11:44 +0200

    modifs ok

--- src/pages/profiles/adminProfile.jsx
+++ src/pages/profiles/adminProfile.jsx
@@ -3,25 +3,96 @@ import { useAtom } from 'jotai';
 import { userAtom } from '../../atoms';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
-import CKEditorComponent from '../../components/CKEditorComponent';
 import ky from 'ky';
 import Cookies from 'js-cookie';
 
 const AdminProfile = () => {
   const [user] = useAtom(userAtom);
   const { t } = useTranslation();
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchRestaurants();
+  }, []);
+
+  const fetchRestaurants = async () => {
+    const token = Cookies.get('token');
+    try {
+      const response = await ky.get('http://localhost:3000/restaurants', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }).json();
+
+      const adminId = parseInt(user.id, 10); // Convertir user.id en nombre
+
+      // Filtrer les restaurants où admin_id correspond à l'ID de l'administrateur connecté
+      const adminRestaurants = response.filter(restaurant => parseInt(restaurant.admin_id, 10) === adminId);
+
+      setRestaurants(adminRestaurants);
+    } catch (error) {
+      console.error('Erreur lors de la récupération des restaurants : ', error);
+    }
+  };
+
+  const handleDelete = async (restaurantId, token) => {
+    try {
+      await ky.delete(`http://localhost:3000/restaurants/${restaurantId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      fetchRestaurants();
+    } catch (error) {
+      console.error('Erreur lors de la suppression du restaurant : ', error);
+    }
+  };
 
   return (
     <div>
-      <h1 className="title-pages"> {t('titleSpaceAdmin')} </h1>
-      <p>{t('email')} : {user.email} </p>
-      <p>{t('id')} : {user.id}</p>
+      <h1 className="title-pages">{t('titleSpaceAdmin')}</h1>
 
-      <Link to="/create-restaurant"> Créer un restaurant </Link>
+      <div className='admin-infos'>
+        <p>{t('email')}: {user.email}</p>
+        <p>{t('id')}: {user.id}</p>
+      </div>
+
+    <div className='link-create'>
+      <Link to="/create-restaurant"> <button> Créer un restaurant </button> </Link>
+    </div>
+
+      <div>
+        <h2 className='your-restau'> Vos restaurants </h2>
+        <div className='cards-admin'>
+        {restaurants.length > 0 ? (
+          restaurants.map(restaurant => (
+            <div key={restaurant.id} className='solo-card'>
+              <h3>{restaurant.name}</h3>
+              <p>{restaurant.description}</p>
+              <p>{restaurant.city}</p>
+              <p>{restaurant.food}</p>
+              <div className='btn-admin'>
+                <Link to={`/edit-restaurant/${restaurant.id}`}>
+                  <button> Éditer </button>
+                </Link>
+                <button onClick={() => handleDelete(restaurant.id)}> Supprimer </button>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p> Aucun restaurant trouvé. </p>
+        )}
+      </div>
+    </div>
     </div>
   );
 };
 
 export default AdminProfile;
+
+
+
+
+
 
 
