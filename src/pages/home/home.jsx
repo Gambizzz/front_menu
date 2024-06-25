68b2364fa4550a0c9d428576e31a04bef45608c8
NicolasBaldo <septfoisept@outlook.fr>
2024-06-25 19:29:16 +0200

    Home2

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,100 +1,85 @@
-"use client";
-
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { Carousel, Accordion } from "flowbite-react";
-import '../../index.scss'
+import { Carousel } from "flowbite-react";
+
+import "../../index.scss";
+
 const Home = () => {
   const { t } = useTranslation();
 
   return (
-    <>
-      <div className="carousel-container">
-        <Carousel>
-          <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="Slide 1" />
-          <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="Slide 2" />
-          <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="Slide 3" />
-          <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="Slide 4" />
-          <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="Slide 5" />
-        </Carousel>
+    <div className="home">
+      {/* Hero Section */}
+      <div className="hero-index">
+        <div className="container my-5">
+          <div className="row">
+            <div className="col-lg-6">
+              <img
+                src="/img_hero_1.png"  // Assurez-vous que le chemin vers votre image est correct
+                className="shadow"
+                alt="Photo d'accueil"
+              />
+            </div>
+            <div className="col-lg-6">
+              <div className="p-5 text-hero">
+                <p className="lead">IL N'A JAMAIS ÉTÉ AUSSI</p>
+                <p className="lead-easier">FACILE</p>
+                <p className="lead">DE TROUVER</p>
+                <p className="lead">CE QUE VOUS ALLEZ</p>
+                <p className="lead-eat">MANGER</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    
-      <Component />
-    </>
+
+      {/* Cities Section */}
+      <div className="cities">
+        <div className="container my-1"> {/* Réduction de la marge de 5 à 3 */}
+          <div className="row">
+            <div className="col-lg-6 text-cities">
+              <div className="p-5 mt-4">
+                <p className="lead">NOUS EXISTONS DÉJÀ</p>
+                <p className="lead-cities">DANS TROIS VILLES...</p>
+                <p className="lead">ET DANS BIEN D'AUTRES</p>
+                <p className="lead-come">À VENIR</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Title and Carousel Section */}
+      <div className="container">
+        <h1 className="home-title">{t('')}</h1>
+        <div className="carousel-container">
+          <Carousel>
+            <img
+              src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+              alt="Slide 1"
+            />
+            <img
+              src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+              alt="Slide 2"
+            />
+            <img
+              src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+              alt="Slide 3"
+            />
+            <img
+              src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+              alt="Slide 4"
+            />
+            <img
+              src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+              alt="Slide 5"
+            />
+          </Carousel>
+        </div>
+      </div>
+    </div>
   );
 };
 
-export function Component() {
-  return (
-    <Accordion>
-      <Accordion.Panel>
-        <Accordion.Title>What is Flowbite?</Accordion.Title>
-        <Accordion.Content>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-            Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons,
-            dropdowns, modals, navbars, and more.
-          </p>
-          <p className="text-gray-500 dark:text-gray-400">
-            Check out this guide to learn how to&nbsp;
-            <a
-              href="https://flowbite.com/docs/getting-started/introduction/"
-              className="text-cyan-600 hover:underline dark:text-cyan-500"
-            >
-              get started&nbsp;
-            </a>
-            and start developing websites even faster with components on top of Tailwind CSS.
-          </p>
-        </Accordion.Content>
-      </Accordion.Panel>
-      <Accordion.Panel>
-        <Accordion.Title>Is there a Figma file available?</Accordion.Title>
-        <Accordion.Content>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-            Flowbite is first conceptualized and designed using the Figma software so everything you see in the library
-            has a design equivalent in our Figma file.
-          </p>
-          <p className="text-gray-500 dark:text-gray-400">
-            Check out the
-            <a href="https://flowbite.com/figma/" className="text-cyan-600 hover:underline dark:text-cyan-500">
-              Figma design system
-            </a>
-            based on the utility classes from Tailwind CSS and components from Flowbite.
-          </p>
-        </Accordion.Content>
-      </Accordion.Panel>
-      <Accordion.Panel>
-        <Accordion.Title>What are the differences between Flowbite and Tailwind UI?</Accordion.Title>
-        <Accordion.Content>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-            The main difference is that the core components from Flowbite are open source under the MIT license, whereas
-            Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone
-            components, whereas Tailwind UI offers sections of pages.
-          </p>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-            However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no
-            technical reason stopping you from using the best of two worlds.
-          </p>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
-          <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
-            <li>
-              <a href="https://flowbite.com/pro/" className="text-cyan-600 hover:underline dark:text-cyan-500">
-                Flowbite Pro
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://tailwindui.com/"
-                rel="nofollow"
-                className="text-cyan-600 hover:underline dark:text-cyan-500"
-              >
-                Tailwind UI
-              </a>
-            </li>
-          </ul>
-        </Accordion.Content>
-      </Accordion.Panel>
-    </Accordion>
-  );
-}
-
 export default Home;
+
