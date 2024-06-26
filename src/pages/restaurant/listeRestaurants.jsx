ea4cde2e34489116c047fa3efa4f7bd48848c3de
NicolasBaldo <septfoisept@outlook.fr>
2024-06-26 11:19:02 +0200

    HOME5

--- src/pages/restaurant/listeRestaurants.jsx
+++ src/pages/restaurant/listeRestaurants.jsx
@@ -104,7 +104,7 @@ const Restaurants = () => {
                 <p className="restaurant-city">{restaurant.city}</p>
                 <p className="restaurant-food">{restaurant.food}</p>
                 <a href="#" className="restaurant-link">
-                  {t('readMore')}
+                  {t('Découvrez son Menu !')}
                   <svg className="restaurant-link-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                   </svg>
