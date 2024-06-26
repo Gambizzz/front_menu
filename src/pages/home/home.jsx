448c03679c941564f670c899bad206a97c77cc1d
NicolasBaldo <septfoisept@outlook.fr>
2024-06-26 10:19:51 +0200

    HOME3

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -11,11 +11,11 @@ const Home = () => {
     <div className="home">
       {/* Hero Section */}
       <div className="hero-index">
-        <div className="container my-5">
+        <div className="container my-9">
           <div className="row">
             <div className="col-lg-6">
               <img
-                src="/img_hero_1.png"  // Assurez-vous que le chemin vers votre image est correct
+                src="/img_hero_1.png" 
                 className="shadow"
                 alt="Photo d'accueil"
               />
@@ -35,12 +35,12 @@ const Home = () => {
 
       {/* Cities Section */}
       <div className="cities">
-        <div className="container my-1"> {/* Réduction de la marge de 5 à 3 */}
+        <div className="container my-1">
           <div className="row">
             <div className="col-lg-6 text-cities">
               <div className="p-5 mt-4">
                 <p className="lead">NOUS EXISTONS DÉJÀ</p>
-                <p className="lead-cities">DANS TROIS VILLES...</p>
+                <p className="lead-cities">DANS ONZE VILLES...</p>
                 <p className="lead">ET DANS BIEN D'AUTRES</p>
                 <p className="lead-come">À VENIR</p>
               </div>
@@ -54,26 +54,50 @@ const Home = () => {
         <h1 className="home-title">{t('')}</h1>
         <div className="carousel-container">
           <Carousel>
-            <img
-              src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
-              alt="Slide 1"
-            />
-            <img
-              src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
-              alt="Slide 2"
-            />
-            <img
-              src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
-              alt="Slide 3"
-            />
-            <img
-              src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
-              alt="Slide 4"
-            />
-            <img
-              src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
-              alt="Slide 5"
-            />
+            <div className="slide">
+              <img src="/PARIS.jpeg" alt="PARIS" />
+              <div className="slide-caption">PARIS</div>
+            </div>
+            <div className="slide">
+              <img src="/MARSEILLE.jpg" alt="MARSEILLE" />
+              <div className="slide-caption">MARSEILLE</div>
+            </div>
+            <div className="slide">
+              <img src="/LYON.avif" alt="LYON" />
+              <div className="slide-caption">LYON</div>
+            </div>
+            <div className="slide">
+              <img src="/STRASBOURG.jpg" alt="STRASBOURG" />
+              <div className="slide-caption">STRASBOURG</div>
+            </div>
+            <div className="slide">
+              <img src="/MONTPEL.jpg" alt="MONTPELLIER" />
+              <div className="slide-caption">MONTPELLIER</div>
+            </div>
+            <div className="slide">
+              <img src="/LILLE.webp" alt="LILLE" />
+              <div className="slide-caption">LILLE</div>
+            </div>
+            <div className="slide">
+              <img src="/RENNES.jpeg" alt="RENNES" />
+              <div className="slide-caption">RENNES</div>
+            </div>
+            <div className="slide">
+              <img src="/BORDEAUX.jpg" alt="BORDEAUX" />
+              <div className="slide-caption">BORDEAUX</div>
+            </div>
+            <div className="slide">
+              <img src="/ROUEN.webp" alt="ROUEN" />
+              <div className="slide-caption">ROUEN</div>
+            </div>
+            <div className="slide">
+              <img src="/NICE.webp" alt="NICE" />
+              <div className="slide-caption">NICE</div>
+            </div>
+            <div className="slide">
+              <img src="/REIMS.avif" alt="REIMS" />
+              <div className="slide-caption">REIMS</div>
+            </div>
           </Carousel>
         </div>
       </div>
@@ -82,4 +106,3 @@ const Home = () => {
 };
 
 export default Home;
-
