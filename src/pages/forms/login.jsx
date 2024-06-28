cac20178b9ae79b0435df500e594d4c724254efe
yeezynico <yeezy77680@gmail.com>
2024-06-28 16:48:05 +0300

    userssssss

--- src/pages/forms/login.jsx
+++ src/pages/forms/login.jsx
@@ -19,7 +19,7 @@ function Log() {
     event.preventDefault();
 
     try {
-      const response = await ky.post(`${api_url}sign_in`, {
+      const response = await ky.post(`${api_url}users/sign_in`, {
         json: {
           user: {
             email,
