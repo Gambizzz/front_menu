69d00935eb4ea6ccf073400556192e9f0e79ffab
Gambizzz <anelisegambini@yahoo.fr>
2024-07-01 00:31:29 +0200

    Responsive ok + delete comment ok

--- src/pages/forms/signup.jsx
+++ src/pages/forms/signup.jsx
@@ -1,16 +1,11 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useAtom } from 'jotai';
-import { userAtom } from '../../atoms';
 import { useTranslation } from 'react-i18next';
-import Cookies from 'js-cookie';
 import ky from 'ky';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { api_url } from '../../App';
 
 function Sign() {
-  const [, setUser] = useAtom(userAtom);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [passwordConfirmation, setPasswordConfirmation] = useState('');
@@ -20,7 +15,7 @@ function Sign() {
     event.preventDefault();
 
     try {
-      const response = await ky.post(`${api_url}users`, {
+      await ky.post(`${api_url}users`, {
         json: {
           user: {
             email,
@@ -30,16 +25,6 @@ function Sign() {
         }
       }).json();
 
-      const { user, token } = response;
-      setUser({
-        email: user.email,
-        id: user.id,
-        token: token,
-        isLoggedIn: true,
-      });
-      Cookies.set('token', token);
-      Cookies.set('id', user.id);
-
       toast.success(t('sign'));
 
       setEmail('');
@@ -47,8 +32,8 @@ function Sign() {
       setPasswordConfirmation('');
 
       setTimeout(() => {
-        window.location.href = "/";
-      }, 1000); 
+        window.location.href = "/login";
+      }, 1000);
 
     } catch (error) {
       console.error('Signup error:', error);
@@ -57,24 +42,24 @@ function Sign() {
   };
 
   return (
-    <div class="signup-form">
-      <form class="form" onSubmit={handleSubmit}>
-        <p class="title">{t('signupForm')}</p>
+    <div className="signup-form">
+      <form className="form" onSubmit={handleSubmit}>
+        <p className="title">{t('signupForm')}</p>
         <label>
-          <input type="email" class="input" id="email" placeholder={t('placeEmail')} value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <input type="email" className="input" id="email" placeholder={t('placeEmail')} value={email} onChange={(e) => setEmail(e.target.value)} required />
         </label>
         <label>
-          <input type="password" class="input" id="password" placeholder={t('password')} value={password} onChange={(e) => setPassword(e.target.value)} required />
+          <input type="password" className="input" id="password" placeholder={t('password')} value={password} onChange={(e) => setPassword(e.target.value)} required />
         </label>
         <label>
-          <input type="password" class="input" id="passwordConfirmation" placeholder={t('passConfirm')} value={passwordConfirmation} onChange={(e) => setPasswordConfirmation(e.target.value)} required />
+          <input type="password" className="input" id="passwordConfirmation" placeholder={t('passConfirm')} value={passwordConfirmation} onChange={(e) => setPasswordConfirmation(e.target.value)} required />
         </label>
-        <button class="submit">{t('welcome')}</button>
-        <p class="signin"> <a href="/login" class="links">{t('seConnecter')}</a> | <a href="/" class="links">{t('home')}</a></p>
+        <button className="submit">{t('welcome')}</button>
+        <p className="signin"> <a href="/login" className="links">{t('seConnecter')}</a> | <a href="/" className="links">{t('home')}</a></p>
       </form>
 
       <ToastContainer />
-      </div>
+    </div>
   );
 }
 
